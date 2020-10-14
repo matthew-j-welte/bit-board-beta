c2d7896b5ba11d47eea942bda5861cdaa5047016
Matthew Welte <matthew.welte95@gmail.com>
2020-10-14 00:44:21 -0700

    creating skeleton controllers for user - account - coding

--- API/Models/Entities/AppUser.cs
+++ API/Models/Entities/AppUser.cs
@@ -1,13 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace API.Models.Entities
 {
-    public class User
+    public class AppUser : IdentityUser<int>
     {
-        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
         public string JobRole { get; set; }
-        public string Email { get; set; }
         public int YearsExperience { get; set; }
         public string CareerSummary { get; set; }
         public string Title { get; set; }
